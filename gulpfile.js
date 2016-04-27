1ec16c50dcf7dfdbcea2cb92a81b4489052fcc7c
Leonard Lamprecht <mindrun@icloud.com>
2016-04-27 12:03:25 +0200

    Build and ignore

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const cache = require('gulp-cached')
+const chmod = require('gulp-chmod')
+
+const path = 'bin/**/*'
+
+gulp.task('transpile', () => {
+  return gulp.src(path)
+  .pipe(cache('transpile'))
+  .pipe(babel())
+  .pipe(chmod(755))
+  .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch(path, ['transpile'])
+})
+
+gulp.task('default', ['watch', 'transpile'])
