8075d8b278523b24f250684bf1312aa254512fcc
Leo Lamprecht <mindrun@icloud.com>
2017-01-15 11:00:10 +0100

    Moved update checking to main binary

--- bin/serve.js
+++ bin/serve.js
@@ -2,9 +2,19 @@
 
 // Packages
 const asyncToGen = require('async-to-gen/register')
+const updateNotifier = require('update-notifier')
+
+// Ours
+const pkg = require('../package')
 
 asyncToGen({
   excludes: null
 })
+
+// Let user know if there's an update
+// This isn't important when deployed to Now
+if (!process.env.NOW) {
+  updateNotifier({pkg}).notify()
+}
 
 require('../lib')
