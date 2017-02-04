d9a4eca267d344c935ee6df2bfb9bf7de1f1d0a1
Leo Lamprecht <mindrun@icloud.com>
2017-02-04 21:24:30 +0100

    Prepare for native async/await without flags

--- bin/serve.js
+++ bin/serve.js
@@ -5,18 +5,21 @@ const asyncToGen = require('async-to-gen/register')
 const updateNotifier = require('update-notifier')
 const {red} = require('chalk')
 const nodeVersion = require('node-version')
+const isAsyncSupported = require('is-async-supported')
 
 // Ours
 const pkg = require('../package')
 
 // Support for keywords "async" and "await"
-const pathSep = process.platform === 'win32' ? '\\\\' : '/'
+if (!isAsyncSupported()) {
+  const pathSep = process.platform === 'win32' ? '\\\\' : '/'
 
-asyncToGen({
-  includes: new RegExp(`.*serve?${pathSep}(lib|bin).*`),
-  excludes: null,
-  sourceMaps: false
-})
+  asyncToGen({
+    includes: new RegExp(`.*serve?${pathSep}(lib|bin).*`),
+    excludes: null,
+    sourceMaps: false
+  })
+}
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
