93be5b1bd7fc4e66d6206674005dc4e133b864b7
Leo Lamprecht <mindrun@icloud.com>
2017-02-14 22:16:21 +0100

    Moved core of package to binary

--- bin/serve.js
+++ bin/serve.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
+// Native
+const path = require('path')
+
 // Packages
+const micro = require('micro')
+const args = require('args')
+const compress = require('micro-compress')
+const detect = require('detect-port')
+const {coroutine} = require('bluebird')
 const updateNotifier = require('update-notifier')
 const {red} = require('chalk')
 const nodeVersion = require('node-version')
 
 // Ours
 const pkg = require('../package')
+const listening = require('../lib/listening')
+const serverHandler = require('../lib/server')
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
@@ -20,4 +30,55 @@ if (!process.env.NOW && pkg.dist) {
   updateNotifier({pkg}).notify()
 }
 
-require('../lib')
+args
+  .option('port', 'Port to listen on', process.env.PORT || 3000)
+  .option('cache', 'How long static files should be cached in the browser (seconds)', 3600)
+  .option('single', 'Serve single page apps with only one index.html')
+  .option('unzipped', 'Disable GZIP compression')
+  .option('ignore', 'Files and directories to ignore', '')
+  .option('auth', 'Serve behind basic auth')
+  .option(['o', 'cors'], 'Setup * CORS headers to allow requests from any origin', false)
+
+const flags = args.parse(process.argv)
+const directory = args.sub[0]
+
+process.env.ASSET_DIR = '/' + Math.random().toString(36).substr(2, 10)
+
+let current = process.cwd()
+
+if (directory) {
+  current = path.resolve(process.cwd(), directory)
+}
+
+let ignoredFiles = [
+  '.DS_Store',
+  '.git/'
+]
+
+if (flags.ignore && flags.ignore.length > 0) {
+  ignoredFiles = ignoredFiles.concat(flags.ignore.split(','))
+}
+
+const handler = coroutine(function * (req, res) {
+  yield serverHandler(req, res, flags, current, ignoredFiles)
+})
+
+const server = flags.unzipped ? micro(handler) : micro(compress(handler))
+let port = flags.port
+
+detect(port).then(open => {
+  let inUse = open !== port
+
+  if (inUse) {
+    port = open
+
+    inUse = {
+      old: flags.port,
+      open
+    }
+  }
+
+  server.listen(port, coroutine(function * () {
+    yield listening(server, current, inUse)
+  }))
+})
