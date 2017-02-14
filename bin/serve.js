90a355d89498d481a19ff8e71597ebfe062a5056
Leo Lamprecht <mindrun@icloud.com>
2017-02-14 22:14:54 +0100

    Use `bluebird` instead of async/await (much faster runtime)

--- bin/serve.js
+++ bin/serve.js
@@ -1,28 +1,12 @@
 #!/usr/bin/env node
 
-// Native
-const path = require('path')
-
 // Packages
-const asyncToGen = require('async-to-gen/register')
 const updateNotifier = require('update-notifier')
 const {red} = require('chalk')
 const nodeVersion = require('node-version')
-const isAsyncSupported = require('is-async-supported')
 
 // Ours
 const pkg = require('../package')
-
-// Support for keywords "async" and "await"
-if (!isAsyncSupported()) {
-  const packageDir = path.basename(path.join(__dirname, '..'))
-  const modulesDir = path.join(packageDir, 'node_modules')
-
-  asyncToGen({
-    excludes: new RegExp(`.*${modulesDir}.*`),
-    sourceMaps: false
-  })
-}
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
@@ -36,5 +20,4 @@ if (!process.env.NOW && pkg.dist) {
   updateNotifier({pkg}).notify()
 }
 
-// Load package core with async/await support
 require('../lib')
