b35865f64c2d95134b92bc6c892090e9cb49f97d
Max Beatty <max@beatty.me>
2017-10-12 12:57:44 -0700

    Introduce tests (#275)
    * add first test
    
    * add more tests

---
+++ test/options.js
@@ -0,0 +1,15 @@
+// Native
+
+// Packages
+const test = require('ava')
+
+// Utilities
+const { options, minimist } = require('../lib/options')
+
+test('options options', t => {
+  t.snapshot(options)
+})
+
+test('options minimist', t => {
+  t.snapshot(minimist)
+})
