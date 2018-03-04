4c181b15f85ddd2de467efdff43b1d4cb86c6197
Tim Neutkens <timneutkens@me.com>
2018-03-05 00:15:15 +0100

    Remove console.log, add test with urlencode

--- test/security.js
+++ test/security.js
@@ -10,6 +10,20 @@ const detect = require('detect-port')
 // Utilities
 const api = require('../lib/api')
 
+test('blocks ignores', async t => {
+  const port = await detect(5000)
+  const { stop } = api(path.join(__dirname, 'fixtures', 'security'), {
+    ignore: ['test.txt'],
+    port
+  })
+
+  await sleep(2500)
+
+  const res = await fetch(`http://localhost:${port}/test.txt`)
+  t.is(res.status, 404)
+  stop()
+})
+
 test('blocks ignores even when requesting urlencoded url', async t => {
   const port = await detect(5000)
   const { stop } = api(path.join(__dirname, 'fixtures', 'security'), {
