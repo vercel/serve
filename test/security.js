dd4a84ee256b4fde2eb259c5c05ea7dc9538826f
Tim Neutkens <tim@timneutkens.nl>
2018-03-05 19:13:01 +0100

    Added tests for `--ignore` flag (#341)
    * Add test for ignore option
    
    * Remove console.log, add test with urlencode
    
    * Remove package-lock.json
    
    * Wait 5s because travis is slower than my laptop
    
    * Refactor tests to be faster / use internal server

---
+++ test/security.js
@@ -0,0 +1,33 @@
+// Native
+const path = require('path')
+
+// Packages
+const test = require('ava')
+const fetch = require('node-fetch')
+const micro = require('micro')
+const listen = require('test-listen')
+
+// Utilities
+const server = require('../lib/server')
+
+const securityAppPath = path.join(__dirname, 'fixtures', 'security')
+
+test('blocks ignores', async t => {
+  const srv = micro((req, res) =>
+    server(req, res, {}, securityAppPath, ['test.txt'])
+  )
+
+  const url = await listen(srv)
+  const res = await fetch(`${url}/test.txt`)
+  t.is(res.status, 404)
+})
+
+test('blocks ignores even when requesting urlencoded url', async t => {
+  const srv = micro((req, res) =>
+    server(req, res, {}, securityAppPath, ['test.txt'])
+  )
+
+  const url = await listen(srv)
+  const res = await fetch(`${url}/t%65st.txt`)
+  t.is(res.status, 404)
+})
