28cc65d008b6a434272d28fa3d93dc09e6b4e198
Leo Lamprecht <mindrun@icloud.com>
2018-04-22 16:07:05 -0700

    Fixed linting

--- bin/serve.js
+++ bin/serve.js
@@ -1,130 +1,130 @@
 #!/usr/bin/env node
 
 // Native
-const path = require('path')
-const https = require('https')
+const path = require('path');
+const https = require('https');
 
 // Packages
-const micro = require('micro')
-const args = require('args')
-const compress = require('micro-compress')
-const detect = require('detect-port')
-const { coroutine } = require('bluebird')
-const checkForUpdate = require('update-check')
-const { red, bold } = require('chalk')
-const nodeVersion = require('node-version')
-const cert = require('openssl-self-signed-certificate')
-const boxen = require('boxen')
+const micro = require('micro');
+const args = require('args');
+const compress = require('micro-compress');
+const detect = require('detect-port');
+const {coroutine} = require('bluebird');
+const checkForUpdate = require('update-check');
+const {red, bold} = require('chalk');
+const nodeVersion = require('node-version');
+const cert = require('openssl-self-signed-certificate');
+const boxen = require('boxen');
 
 // Utilities
-const pkg = require('../package')
-const listening = require('../lib/listening')
-const serverHandler = require('../lib/server')
-const { options, minimist } = require('../lib/options')
+const pkg = require('../package');
+const listening = require('../lib/listening');
+const serverHandler = require('../lib/server');
+const {options, minimist} = require('../lib/options');
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
-  console.error(
-    `${red(
-      'Error!'
-    )} Serve requires at least version 6 of Node. Please upgrade!`
-  )
-  process.exit(1)
+	console.error(
+		`${red(
+			'Error!'
+		)} Serve requires at least version 6 of Node. Please upgrade!`
+	);
+	process.exit(1);
 }
 
 // Register the list of options
-args.options(options)
+args.options(options);
 
 // And initialize `args`
-const flags = args.parse(process.argv, { minimist })
+const flags = args.parse(process.argv, {minimist});
 
 // Figure out the content directory
-const [directory] = args.sub
+const [directory] = args.sub;
 
 // Don't log anything to the console if silent mode is enabled
 if (flags.silent) {
-  console.log = () => {}
+	console.log = () => {};
 }
 
 process.env.ASSET_DIR = Math.random()
-  .toString(36)
-  .substr(2, 10)
+	.toString(36)
+	.substr(2, 10);
 
-let current = process.cwd()
+let current = process.cwd();
 
 if (directory) {
-  current = path.resolve(process.cwd(), directory)
+	current = path.resolve(process.cwd(), directory);
 }
 
-let ignoredFiles = ['.DS_Store', '.git/']
+let ignoredFiles = ['.DS_Store', '.git/'];
 
 if (flags.ignore && flags.ignore.length > 0) {
-  ignoredFiles = ignoredFiles.concat(flags.ignore.split(','))
+	ignoredFiles = ignoredFiles.concat(flags.ignore.split(','));
 }
 
-const handler = coroutine(function*(req, res) {
-  yield serverHandler(req, res, flags, current, ignoredFiles)
-})
+const handler = coroutine(function *run(req, res) {
+	yield serverHandler(req, res, flags, current, ignoredFiles);
+});
 
 const httpsOpts = {
-  key: cert.key,
-  cert: cert.cert,
-  passphrase: cert.passphrase
-}
+	key: cert.key,
+	cert: cert.cert,
+	passphrase: cert.passphrase
+};
 
 const microHttps = fn =>
-  https.createServer(httpsOpts, (req, res) => micro.run(req, res, fn))
+	https.createServer(httpsOpts, (req, res) => micro.run(req, res, fn));
 const server = flags.ssl
-  ? microHttps(flags.unzipped ? handler : compress(handler))
-  : micro(flags.unzipped ? handler : compress(handler))
+	? microHttps(flags.unzipped ? handler : compress(handler))
+	: micro(flags.unzipped ? handler : compress(handler));
 
-let { port } = flags
+let {port} = flags;
 
 detect(port).then(async open => {
-  const { NODE_ENV } = process.env
+	const {NODE_ENV} = process.env;
 
-  if (NODE_ENV !== 'production') {
-    const update = await checkForUpdate(pkg)
+	if (NODE_ENV !== 'production') {
+		const update = await checkForUpdate(pkg);
 
-    if (update) {
-      const message = `${bold(
-        'UPDATE AVAILABLE:'
-      )} The latest version of \`serve\` is ${update.latest}`
-  
-      console.log(
-        boxen(message, {
-          padding: 1,
-          borderColor: 'green',
-          margin: 1
-        })
-      )
-    }
-  }
+		if (update) {
+			const message = `${bold(
+				'UPDATE AVAILABLE:'
+			)} The latest version of \`serve\` is ${update.latest}`;
 
-  let inUse = open !== port
+			console.log(
+				boxen(message, {
+					padding: 1,
+					borderColor: 'green',
+					margin: 1
+				})
+			);
+		}
+	}
 
-  if (inUse) {
-    port = open
+	let inUse = open !== port;
 
-    inUse = {
-      old: flags.port,
-      open
-    }
-  }
+	if (inUse) {
+		port = open;
 
-  const listenArgs = [
-    server,
-    current,
-    inUse,
-    flags.clipless !== true,
-    flags.open,
-    flags.ssl,
-    flags.local
-  ]
+		inUse = {
+			old: flags.port,
+			open
+		};
+	}
 
-  if (flags.local) {
-    server.listen(port, 'localhost', listening.bind(this, ...listenArgs))
-  } else {
-    server.listen(port, listening.bind(this, ...listenArgs))
-  }
-})
+	const listenArgs = [
+		server,
+		current,
+		inUse,
+		flags.clipless !== true,
+		flags.open,
+		flags.ssl,
+		flags.local
+	];
+
+	if (flags.local) {
+		server.listen(port, 'localhost', listening.bind(this, ...listenArgs));
+	} else {
+		server.listen(port, listening.bind(this, ...listenArgs));
+	}
+});
