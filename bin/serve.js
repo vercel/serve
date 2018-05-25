1c0d2e4692fef5bd938dcdec9045f7caddf62a35
Leo Lamprecht <mindrun@icloud.com>
2018-05-25 11:40:37 +0200

    Added update checking

--- bin/serve.js
+++ bin/serve.js
@@ -1,143 +1,46 @@
 #!/usr/bin/env node
 
-// Native
-const path = require('path');
-const https = require('https');
-
 // Packages
-const micro = require('micro');
-const args = require('args');
-const compress = require('micro-compress');
-const detect = require('detect-port');
-const {coroutine} = require('bluebird');
-const checkForUpdate = require('update-check');
-const {red, bold} = require('chalk');
-const nodeVersion = require('node-version');
-const cert = require('openssl-self-signed-certificate');
 const boxen = require('boxen');
+const checkForUpdate = require('update-check');
+const {bold} = require('chalk');
 
 // Utilities
 const pkg = require('../package');
-const listening = require('../lib/listening');
-const serverHandler = require('../lib/server');
-const {options, minimist} = require('../lib/options');
 
-// Throw an error if node version is too low
-if (nodeVersion.major < 6) {
-	console.error(
-		`${red(
-			'Error!'
-		)} Serve requires at least version 6 of Node. Please upgrade!`
-	);
-	process.exit(1);
-}
+const {NODE_ENV} = process.env;
 
-// Register the list of options
-args.options(options);
+const updateCheck = async () => {
+	if (NODE_ENV === 'production') {
+		return;
+	}
 
-// And initialize `args`
-const flags = args.parse(process.argv, {minimist});
+	const boxenConfig = {
+		padding: 1,
+		margin: 1,
+		borderColor: 'green'
+	};
 
-// Figure out the content directory
-const [directory] = args.sub;
+	let update = null;
 
-// Don't log anything to the console if silent mode is enabled
-if (flags.silent) {
-	console.log = () => {};
-}
+	try {
+		update = await checkForUpdate(pkg);
+	} catch (err) {
+		const message = `${bold('UPDATE CHECK FAILED:')} ${err.message}`;
 
-process.env.ASSET_DIR = Math.random()
-	.toString(36)
-	.substr(2, 10);
+		console.error(boxen(message, Object.assign({}, boxenConfig, {
+			borderColor: 'red'
+		})));
+	}
 
-let current = process.cwd();
+	if (!update) {
+		return;
+	}
 
-if (directory) {
-	current = path.resolve(process.cwd(), directory);
-}
-
-let ignoredFiles = ['.DS_Store', '.git/'];
-
-if (flags.ignore && flags.ignore.length > 0) {
-	ignoredFiles = ignoredFiles.concat(flags.ignore.split(','));
-}
-
-const handler = coroutine(function *run(req, res) {
-	yield serverHandler(req, res, flags, current, ignoredFiles);
-});
-
-const httpsOpts = {
-	key: cert.key,
-	cert: cert.cert
+	const message = `${bold('UPDATE AVAILABLE:')} The latest version of \`serve\` is ${update.latest}`;
+	console.error(boxen(message, boxenConfig));
 };
 
-const microHttps = fn =>
-	https.createServer(httpsOpts, (req, res) => micro.run(req, res, fn));
-const server = flags.ssl
-	? microHttps(flags.unzipped ? handler : compress(handler))
-	: micro(flags.unzipped ? handler : compress(handler));
-
-let {port} = flags;
-
-detect(port).then(async open => {
-	const {NODE_ENV} = process.env;
-
-	if (NODE_ENV !== 'production') {
-		try {
-			const update = await checkForUpdate(pkg);
-
-			if (update) {
-				const message = `${bold(
-					'UPDATE AVAILABLE:'
-				)} The latest version of \`serve\` is ${update.latest}`;
-
-				console.log(
-					boxen(message, {
-						padding: 1,
-						borderColor: 'green',
-						margin: 1
-					})
-				);
-			}
-		} catch (err) {
-			console.log(
-				boxen(
-					`${bold(
-						'UPDATE CHECK FAILED:'
-					)} ${err.message}`, {
-						padding: 1,
-						borderColor: 'red',
-						margin: 1
-					}
-				)
-			);
-		}
-	}
-
-	let inUse = open !== port;
-
-	if (inUse) {
-		port = open;
-
-		inUse = {
-			old: flags.port,
-			open
-		};
-	}
-
-	const listenArgs = [
-		server,
-		current,
-		inUse,
-		flags.clipless !== true,
-		flags.open,
-		flags.ssl,
-		flags.local
-	];
-
-	if (flags.local) {
-		server.listen(port, 'localhost', listening.bind(this, ...listenArgs));
-	} else {
-		server.listen(port, listening.bind(this, ...listenArgs));
-	}
-});
+(async () => {
+	await updateCheck();
+})();
