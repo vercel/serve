395830d43349ed5bc0633c3f44c6cd706b50be3a
Leo Lamprecht <mindrun@icloud.com>
2018-05-28 19:05:43 +0200

    Rewrite project entirely (#374)
    * Removed useless directories and renamed files
    
    * Removed last remaining directory
    
    * Removed unused dependencies
    
    * Added update checking
    
    * Added beautiful help
    
    * Added basic serving functionality
    
    * Got rid of weird style
    
    * Renamed license file
    
    * Adjusted readme to new core
    
    * Made clear what's required
    
    * Be clear that it behaves like Now
    
    * Be even more clear
    
    * Perfected intro
    
    * Cleaned up the readme
    
    * Ensured linting is working
    
    * Upload only what's necessary
    
    * Made line shorter
    
    * Allow for custom configuration
    
    * Made logged messages perfect
    
    * Log sweet error when wrong argument is provided
    
    * Upgraded serve-handler
    
    * Upgraded serve-handler again to fix bugs
    
    * Allow force-closing
    
    * Ensured middleware as it its latest

---
+++ README.md
@@ -0,0 +1,58 @@
+# serve
+
+[![Build Status](https://circleci.com/gh/zeit/serve.svg?&style=shield)](https://circleci.com/gh/zeit/serve)
+[![Join the community on Spectrum](https://withspectrum.github.io/badge/badge.svg)](https://spectrum.chat/micro/serve)
+
+Assuming you would like to serve a static site, single page application or just a static file (no matter if on your device or on the local network), this package is just the right choice for you.
+
+It behaves exactly like static deployments on [Now](https://zeit.co/now), so it's perfect for developing your static project. Then, when it's time to push it into production, you [deploy it](https://zeit.co/docs/examples/static).
+
+Furthermore, it also provides a neat interface for listing the directory's contents:
+
+![screenshot](https://user-images.githubusercontent.com/6170607/40541195-167ff460-601b-11e8-8f66-3b0c7ff96cbb.png)
+
+## Usage
+
+Firstly, install the package using [Yarn](https://yarnpkg.com/en/) (you'll need at least Node.js LTS):
+
+```bash
+yarn global add serve
+```
+
+Once that's done, you can run this command inside your project's directory:
+
+```bash
+serve
+```
+
+Finally, run this command to see a list of all available options:
+
+```bash
+serve --help
+```
+
+Now you understand how the package works! :tada:
+
+## Configuration
+
+To customize `serve`'s behavior, create a `serve.json` file and insert any of [these properties](https://github.com/zeit/serve-handler#options). In addition, `serve` will also detect `now.json` files if they contain the `static` property.
+
+## API
+
+The core of `serve` is [serve-handler](https://github.com/zeit/serve-handler), which can be used as middleware in existing HTTP servers.
+
+## Contributing
+
+1. [Fork](https://help.github.com/articles/fork-a-repo/) this repository to your own GitHub account and then [clone](https://help.github.com/articles/cloning-a-repository/) it to your local device
+2. Uninstall `serve` if it's already installed: `yarn global remove serve`
+3. Link it to the global module directory: `yarn link`
+
+After that, you can use the `serve` command everywhere. [Here](https://github.com/zeit/serve/issues?q=is%3Aissue+is%3Aopen+label%3A%22good+for+beginners%22)'s a list of issues that are great for beginners.
+
+## Credits
+
+This project used to be called "list" and "micro-list". But thanks to [TJ Holowaychuk](https://github.com/tj) handing us the new name, it's now called "serve" (which is much more definite).
+
+## Author
+
+Leo Lamprecht ([@notquiteleo](https://twitter.com/notquiteleo)) - [ZEIT](https://zeit.co)
--- bin/serve.js
+++ bin/serve.js
@@ -2,142 +2,260 @@
 
 // Native
 const path = require('path');
-const https = require('https');
 
 // Packages
-const micro = require('micro');
-const args = require('args');
-const compress = require('micro-compress');
-const detect = require('detect-port');
-const {coroutine} = require('bluebird');
+const Ajv = require('ajv');
+const dotProp = require('dot-prop');
 const checkForUpdate = require('update-check');
-const {red, bold} = require('chalk');
-const nodeVersion = require('node-version');
-const cert = require('openssl-self-signed-certificate');
-const boxen = require('boxen');
+const chalk = require('chalk');
+const micro = require('micro');
+const arg = require('arg');
+const handler = require('serve-handler');
+const schema = require('@zeit/schemas/deployment/config-static');
+const fs = require('fs-extra');
 
 // Utilities
 const pkg = require('../package');
-const listening = require('../lib/listening');
-const serverHandler = require('../lib/server');
-const {options, minimist} = require('../lib/options');
 
-// Throw an error if node version is too low
-if (nodeVersion.major < 6) {
-	console.error(
-		`${red(
-			'Error!'
-		)} Serve requires at least version 6 of Node. Please upgrade!`
-	);
-	process.exit(1);
-}
+const warning = message => chalk`{yellow WARNING:} ${message}`;
+const info = message => chalk`{magenta INFO:} ${message}`;
+const error = message => chalk`{red ERROR:} ${message}`;
 
-// Register the list of options
-args.options(options);
+const updateCheck = async isDebugging => {
+	let update = null;
 
-// And initialize `args`
-const flags = args.parse(process.argv, {minimist});
+	try {
+		update = await checkForUpdate(pkg);
+	} catch (err) {
+		const suffix = isDebugging ? ':' : ' (use `--debug` to see full error)';
+		console.error(warning(`Checking for updates failed${suffix}`));
 
-// Figure out the content directory
-const [directory] = args.sub;
-
-// Don't log anything to the console if silent mode is enabled
-if (flags.silent) {
-	console.log = () => {};
-}
-
-process.env.ASSET_DIR = Math.random()
-	.toString(36)
-	.substr(2, 10);
-
-let current = process.cwd();
-
-if (directory) {
-	current = path.resolve(process.cwd(), directory);
-}
-
-let ignoredFiles = ['.DS_Store', '.git/'];
-
-if (flags.ignore && flags.ignore.length > 0) {
-	ignoredFiles = ignoredFiles.concat(flags.ignore.split(','));
-}
-
-const handler = coroutine(function *run(req, res) {
-	yield serverHandler(req, res, flags, current, ignoredFiles);
-});
-
-const httpsOpts = {
-	key: cert.key,
-	cert: cert.cert
-};
-
-const microHttps = fn =>
-	https.createServer(httpsOpts, (req, res) => micro.run(req, res, fn));
-const server = flags.ssl
-	? microHttps(flags.unzipped ? handler : compress(handler))
-	: micro(flags.unzipped ? handler : compress(handler));
-
-let {port} = flags;
-
-detect(port).then(async open => {
-	const {NODE_ENV} = process.env;
-
-	if (NODE_ENV !== 'production') {
-		try {
-			const update = await checkForUpdate(pkg);
-
-			if (update) {
-				const message = `${bold(
-					'UPDATE AVAILABLE:'
-				)} The latest version of \`serve\` is ${update.latest}`;
-
-				console.log(
-					boxen(message, {
-						padding: 1,
-						borderColor: 'green',
-						margin: 1
-					})
-				);
-			}
-		} catch (err) {
-			console.log(
-				boxen(
-					`${bold(
-						'UPDATE CHECK FAILED:'
-					)} ${err.message}`, {
-						padding: 1,
-						borderColor: 'red',
-						margin: 1
-					}
-				)
-			);
+		if (isDebugging) {
+			console.error(err);
 		}
 	}
 
-	let inUse = open !== port;
-
-	if (inUse) {
-		port = open;
-
-		inUse = {
-			old: flags.port,
-			open
-		};
+	if (!update) {
+		return;
 	}
 
-	const listenArgs = [
-		server,
-		current,
-		inUse,
-		flags.clipless !== true,
-		flags.open,
-		flags.ssl,
-		flags.local
-	];
+	console.log(`${chalk.bgRed('UPDATE AVAILABLE')} The latest version of \`serve\` is ${update.latest}`);
+};
 
-	if (flags.local) {
-		server.listen(port, 'localhost', listening.bind(this, ...listenArgs));
-	} else {
-		server.listen(port, listening.bind(this, ...listenArgs));
+const getHelp = () => chalk`
+  {bold.cyan serve} - Static file serving and directory listing
+
+  {bold USAGE}
+
+      {bold $} {cyan serve} --help
+      {bold $} {cyan serve} --version
+      {bold $} {cyan serve} [-l {underline listen_uri} [-l ...]] [{underline directory}]
+
+      By default, {cyan serve} will listen on {bold 0.0.0.0:3000} and serve the
+      current working directory on that address.
+
+      Specifying a single {bold --listen} argument will overwrite the default, not supplement it.
+
+  {bold OPTIONS}
+
+      --help                              Shows this help message
+
+      -v, --version                       Displays the current version of serve
+
+      -l, --listen {underline listen_uri}             Specify a URI endpoint on which to listen (see below) -
+                                          more than one may be specified to listen in multiple places
+
+      -d, --debug                         Show debugging information
+
+  {bold ENDPOINTS}
+
+      Listen endpoints (specified by the {bold --listen} or {bold -l} options above) instruct {cyan micro}
+      to listen on one or more interfaces/ports, UNIX domain sockets, or Windows named pipes.
+
+      For TCP (traditional host/port) endpoints:
+
+          {bold $} {cyan serve} -l tcp://{underline hostname}:{underline 1234}
+
+      For UNIX domain socket endpoints:
+
+          {bold $} {cyan serve} -l unix:{underline /path/to/socket.sock}
+
+      For Windows named pipe endpoints:
+
+          {bold $} {cyan serve} -l pipe:\\\\.\\pipe\\{underline PipeName}
+`;
+
+const parseEndpoint = str => {
+	const url = new URL(str);
+
+	switch (url.protocol) {
+	case 'pipe:': {
+		// some special handling
+		const cutStr = str.replace(/^pipe:/, '');
+
+		if (cutStr.slice(0, 4) !== '\\\\.\\') {
+			throw new Error(`Invalid Windows named pipe endpoint: ${str}`);
+		}
+
+		return [cutStr];
 	}
-});
+	case 'unix:':
+		if (!url.pathname) {
+			throw new Error(`Invalid UNIX domain socket endpoint: ${str}`);
+		}
+
+		return [url.pathname];
+	case 'tcp:':
+		url.port = url.port || '3000';
+		return [parseInt(url.port, 10), url.hostname];
+	default:
+		throw new Error(`Unknown --listen endpoint scheme (protocol): ${url.protocol}`);
+	}
+};
+
+const registerShutdown = fn => {
+	let run = false;
+
+	const wrapper = () => {
+		if (!run) {
+			run = true;
+			fn();
+		}
+	};
+
+	process.on('SIGINT', wrapper);
+	process.on('SIGTERM', wrapper);
+	process.on('exit', wrapper);
+};
+
+const startEndpoint = (endpoint, config) => {
+	const server = micro(async (request, response) => handler(request, response, config));
+
+	server.on('error', err => {
+		console.error('serve:', err.stack);
+		process.exit(1);
+	});
+
+	server.listen(...endpoint, () => {
+		const details = server.address();
+
+		registerShutdown(() => server.close());
+
+		// `micro` is designed to run only in production, so
+		// this message is perfectly for prod
+		if (typeof details === 'string') {
+			console.log(info(`Accepting connections at ${details}`));
+		} else if (typeof details === 'object' && details.port) {
+			console.log(info(`Accepting connections at http://localhost:${details.port}`));
+		} else {
+			console.log(info('Accepting connections'));
+		}
+	});
+};
+
+const loadConfig = async (cwd, entry) => {
+	const paths = {
+		'serve.json': null,
+		'now.json': 'static',
+		'package.json': 'now.static'
+	};
+
+	const config = {};
+
+	for (const file of Object.keys(paths)) {
+		const location = path.join(entry, file);
+		const prop = paths[file];
+
+		try {
+			const content = await fs.readJSON(location);
+			Object.assign(config, prop ? dotProp.get(content, prop) : content);
+		} catch (err) {
+			continue;
+		}
+
+		if (Object.keys(config).length !== 0) {
+			console.log(info(`Discovered configuration in \`${file}\``));
+			break;
+		}
+	}
+
+	if (entry) {
+		const {public} = config;
+		config.public = path.relative(cwd, (public ? path.join(entry, public) : entry));
+	}
+
+	if (Object.keys(config).length !== 0) {
+		const ajv = new Ajv();
+		const validateSchema = ajv.compile(schema);
+
+		if (!validateSchema(config)) {
+			const defaultMessage = error('The configuration you provided is wrong:');
+			const {message, params} = validateSchema.errors[0];
+
+			console.error(`${defaultMessage}\n${message}\n${JSON.stringify(params)}`);
+			process.exit(1);
+		}
+	}
+
+	return config;
+};
+
+(async () => {
+	let args = null;
+
+	try {
+		args = arg({
+			'--help': Boolean,
+			'--version': Boolean,
+			'--listen': [parseEndpoint],
+			'--debug': Boolean,
+			'-h': '--help',
+			'-v': '--version',
+			'-l': '--listen',
+			'-d': '--debug'
+		});
+	} catch (err) {
+		console.error(error(err.message));
+		process.exit(1);
+	}
+
+	await updateCheck(args['--debug']);
+
+	if (args['--version']) {
+		console.log(pkg.version);
+		return;
+	}
+
+	if (args['--help']) {
+		console.log(getHelp());
+		return;
+	}
+
+	if (!args['--listen']) {
+		// Default endpoint
+		args['--listen'] = [[3000]];
+	}
+
+	if (args._.length > 1) {
+		console.error(error('Please provide one path argument at maximum'));
+		process.exit(1);
+	}
+
+	const cwd = process.cwd();
+	const entry = args._.length > 0 ? path.join(cwd, args._[0]) : cwd;
+
+	const config = await loadConfig(cwd, entry);
+
+	for (const endpoint of args['--listen']) {
+		startEndpoint(endpoint, config);
+	}
+
+	registerShutdown(() => {
+		console.log(`\n${info('Gracefully shutting down. Please wait...')}`);
+
+		process.on('SIGINT', () => {
+			console.log(`\n${warning('Force-closing all open sockets...')}`);
+			process.exit(0);
+		});
+	});
+})();
