58a8eab0c7110a31d942042f1ad3e3e111bcc30a
Leo Lamprecht <mindrun@icloud.com>
2018-05-28 13:46:35 +0200

    Allow for custom configuration

--- bin/serve.js
+++ bin/serve.js
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
 
+// Native
+const path = require('path');
+
 // Packages
+const Ajv = require('ajv');
+const dotProp = require('dot-prop');
 const checkForUpdate = require('update-check');
 const chalk = require('chalk');
 const micro = require('micro');
 const arg = require('arg');
 const handler = require('serve-handler');
+const schema = require('@zeit/schemas/deployment/config-static');
+const fs = require('fs-extra');
 
 // Utilities
 const pkg = require('../package');
 
 const warning = message => chalk`{yellow WARNING:} ${message}`;
+const info = message => chalk`{magenta INFO:} ${message}`;
+const error = message => chalk`{red ERROR:} ${message}`;
 
 const updateCheck = async isDebugging => {
 	let update = null;
@@ -119,8 +128,8 @@ const registerShutdown = fn => {
 	process.on('exit', wrapper);
 };
 
-const startEndpoint = endpoint => {
-	const server = micro(async (request, response) => handler(request, response));
+const startEndpoint = (endpoint, config) => {
+	const server = micro(async (request, response) => handler(request, response, config));
 
 	server.on('error', err => {
 		console.error('serve:', err.stack);
@@ -142,6 +151,51 @@ const startEndpoint = endpoint => {
 			console.log('serve: Accepting connections');
 		}
 	});
+};
+
+const loadConfig = async (cwd, entry) => {
+	const paths = {
+		'serve.json': null,
+		'now.json': 'static',
+		'package.json': 'now.static'
+	};
+
+	const config = {};
+
+	for (const file of Object.keys(paths)) {
+		const location = path.join(entry, file);
+		const prop = paths[file];
+
+		try {
+			const content = await fs.readJSON(location);
+			Object.assign(config, prop ? dotProp.get(content, prop) : content);
+			console.log(info(`Discovered configuration in ${file}`));
+
+			break;
+		} catch (err) {
+			continue;
+		}
+	}
+
+	if (entry) {
+		const {public} = config;
+		config.public = path.relative(cwd, (public ? path.join(entry, public) : entry));
+	}
+
+	if (Object.keys(config).length !== 0) {
+		const ajv = new Ajv();
+		const validateSchema = ajv.compile(schema);
+
+		if (!validateSchema(config)) {
+			const defaultMessage = error('The configuration you provided is wrong:');
+			const {message, params} = validateSchema.errors[0];
+
+			console.error(`${defaultMessage}\n${message}\n${JSON.stringify(params)}`);
+			process.exit(1);
+		}
+	}
+
+	return config;
 };
 
 (async () => {
@@ -173,8 +227,18 @@ const startEndpoint = endpoint => {
 		args['--listen'] = [[3000]];
 	}
 
+	if (args._.length > 1) {
+		console.error(error('Please provide one path argument at maximum'));
+		process.exit(1);
+	}
+
+	const cwd = process.cwd();
+	const entry = args._.length > 0 ? path.join(cwd, args._[0]) : cwd;
+
+	const config = await loadConfig(cwd, entry);
+
 	for (const endpoint of args['--listen']) {
-		startEndpoint(endpoint);
+		startEndpoint(endpoint, config);
 	}
 
 	registerShutdown(() => console.log('serve: Gracefully shutting down. Please wait...'));
