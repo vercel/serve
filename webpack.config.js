632b98cb9f0e662ee3c8ccfda5206ba7bddf7235
Thach Nguyen <nthachus@gmail.com>
2020-08-07 17:17:40 +0700

    - Use `webpack` to bundle package with all dependencies

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPkgJsonPlugin = require('copy-pkg-json-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	mode: 'production',
+	entry: './bin/serve',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'index.js'
+	},
+	context: __dirname,
+	target: 'node',
+	node: {
+		__filename: false,
+		__dirname: false
+	},
+	externals: ['spawn-sync'],
+	module: {
+		rules: [
+			{
+				test: path.resolve(__dirname, 'bin/serve.js'),
+				loader: 'string-replace-loader',
+				options: { search: '^#!.*[\\r\\n]+', flags: '', replace: '' }
+			},
+			{
+				test: /node_modules.clipboardy\b.*\.js$/,
+				loader: 'string-replace-loader',
+				options: { search: '\\.\\./(fallbacks/)', flags: 'g', replace: '$1' }
+			},
+			{
+				// transpile ES6-8 into ES5
+				test: /\.m?js$/,
+				exclude: /node_modules.(ajv|color-convert|cross-spawn|debug|isexe|negotiator|signal-exit|uri-js)\b/,
+				loader: 'babel-loader',
+				options: {
+					cacheDirectory: true,
+					presets: [['@babel/preset-env', { targets: { node: true } }]] // esmodules
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+		new CopyPkgJsonPlugin({
+			remove: ['dependencies', 'resolutions', 'files'],
+			replace: { bin: { serve: './index.js' } }
+		}),
+		new CopyPlugin([
+			{ from: '{LICENSE,README.md}' },
+			{ from: 'node_modules/clipboardy/fallbacks', to: 'fallbacks/', ignore: ['.DS_Store'] },
+			{ from: 'node_modules/term-size/vendor', to: 'vendor/' }
+		])
+	],
+	optimization: {
+		nodeEnv: false,
+		// minimize: false,
+		minimizer: [
+			new TerserPlugin({
+				cache: true,
+				parallel: true,
+				extractComments: { condition: /^\**!|@preserve|@license|@cc_on/i, banner: false }
+			})
+		]
+	}
+};
