06f9665a4117a5e12ee1dffeb38729d77590df71
Vedant K <gamemaker0042@gmail.com>
2022-07-11 16:12:10 +0530

    fix(types): remove some `@ts-expect-error` comments

--- source/utilities/server.ts
+++ source/utilities/server.ts
@@ -36,33 +36,46 @@ export const startServer = async (
   previous?: Port,
 ): Promise<ServerAddress> => {
   // Define the request handler for the server.
-  const serverHandler = async (
+  const serverHandler = (
     request: IncomingMessage,
     response: ServerResponse,
-  ): Promise<void> => {
-    if (args['--cors']) response.setHeader('Access-Control-Allow-Origin', '*');
-    // @ts-expect-error The `compression` library uses the Express Request type
-    // instead of the native HTTP IncomingMessage type.
-    if (!args['--no-compression']) await compress(request, response);
+  ): void => {
+    // We can't return a promise in a HTTP request handler, so we run our code
+    // inside an async function instead.
+    const run = async () => {
+      type ExpressRequest = Parameters<typeof compress>[0];
+      type ExpressResponse = Parameters<typeof compress>[1];
 
-    // Let the `serve-handler` module do the rest.
-    await handler(request, response, config);
+      if (args['--cors'])
+        response.setHeader('Access-Control-Allow-Origin', '*');
+      if (!args['--no-compression'])
+        await compress(request as ExpressRequest, response as ExpressResponse);
+
+      // Let the `serve-handler` module do the rest.
+      await handler(request, response, config);
+    };
+
+    // Then we run the async function, and re-throw any errors.
+    run().catch((error: Error) => {
+      throw error;
+    });
   };
 
   // Create the server.
   const httpMode = args['--ssl-cert'] && args['--ssl-key'] ? 'https' : 'http';
   const sslPass = args['--ssl-pass'];
+  const serverConfig =
+    httpMode === 'https'
+      ? {
+          key: await readFile(args['--ssl-key']),
+          cert: await readFile(args['--ssl-cert']),
+          passphrase: sslPass ? await readFile(sslPass, 'utf8') : '',
+        }
+      : {};
   const server =
     httpMode === 'https'
-      ? https.createServer(
-          {
-            key: await readFile(args['--ssl-key']),
-            cert: await readFile(args['--ssl-cert']),
-            passphrase: sslPass ? await readFile(sslPass, 'utf8') : '',
-          },
-          serverHandler, // eslint-disable-line @typescript-eslint/no-misused-promises
-        )
-      : http.createServer(serverHandler); // eslint-disable-line @typescript-eslint/no-misused-promises
+      ? https.createServer(serverConfig, serverHandler)
+      : http.createServer(serverHandler);
 
   // Once the server starts, return the address it is running on so the CLI
   // can tell the user.
@@ -78,8 +91,9 @@ export const startServer = async (
     if (typeof details === 'string') {
       local = details;
     } else if (typeof details === 'object' && details.port) {
-      // According to https://www.ietf.org/rfc/rfc2732.txt, IPv6 address should be
-      // surrounded by square brackets (only the address, not the port).
+      // According to https://www.ietf.org/rfc/rfc2732.txt, IPv6 addresses
+      // should be surrounded by square brackets (only the address, not the
+      // port).
       let address;
       if (details.address === '::') address = 'localhost';
       else if (details.family === 'IPv6') address = `[${details.address}]`;
@@ -121,8 +135,20 @@ export const startServer = async (
 
   // Finally, start the server.
   return new Promise((resolve, _reject) => {
-    // @ts-expect-error I'm not sure why Typescript thinks this is invalid, the spread
-    // operator should pass it a `number` or a `string` or a `number`, `string`.
-    server.listen(...endpoint, () => resolve(getServerDetails()));
+    // If only a port is specified, listen on the given port on localhost.
+    if (endpoint.length === 1 && typeof endpoint[0] === 'number')
+      server.listen(endpoint[0], () => resolve(getServerDetails()));
+    // If the path to a socket or a pipe is given, listen on it.
+    else if (endpoint.length === 1 && typeof endpoint[0] === 'string')
+      server.listen(endpoint[0], () => resolve(getServerDetails()));
+    // If a port number and hostname are given, listen on `host:port`.
+    else if (
+      endpoint.length === 2 &&
+      typeof endpoint[0] === 'number' &&
+      typeof endpoint[1] === 'string'
+    )
+      server.listen(endpoint[0], endpoint[1], () =>
+        resolve(getServerDetails()),
+      );
   });
 };
