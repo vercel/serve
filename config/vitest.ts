ffcd4c90b5cf2f8c50a6af5cdc4818caca751e10
Vedant K <gamemaker0042@gmail.com>
2022-07-15 15:34:48 +0530

    test: force color output for tests

--- config/vitest.ts
+++ config/vitest.ts
@@ -1,7 +1,12 @@
 // config/vitest.ts
 // The vitest configuration file.
 
+import { env } from 'node:process';
 import { defineConfig } from 'vitest/config';
+
+// Make sure the output of the CLI is in color, so that it matches the
+// snapshots.
+env.FORCE_COLOR = 2;
 
 export default defineConfig({
   test: {
